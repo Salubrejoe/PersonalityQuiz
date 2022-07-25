1d562e4733f1a59c8e528ed7d1d1dd33b9be3812
Licurgen <44895517+Salubrejoe@users.noreply.github.com>
2022-07-25 17:44:24 +0100

    Initial Commit

---
+++ Personality Quiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Personality Quiz
+//
+//  Created by Lore P on 25/07/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
