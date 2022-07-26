72d266809580b3534a8ce9626a1c51812daed9e1
Licurgen <44895517+Salubrejoe@users.noreply.github.com>
2022-07-26 18:17:24 +0100

    definitions of modes

--- Personality Quiz/QuestionViewController.swift
+++ Personality Quiz/QuestionViewController.swift
@@ -9,21 +9,48 @@ import UIKit
 
 class QuestionViewController: UIViewController {
 
+    @IBOutlet weak var singleStackView: UIStackView!
+    @IBOutlet weak var multipleStackView: UIStackView!
+    @IBOutlet weak var rangedStackView: UIStackView!
+    
+    
+    var questions : [Question] =
+    
+    [Question(text: "Which food do you like the most", type: .single,
+              Answer: [
+                Answer(text: "Fruit", type: .turtle),
+                Answer(text: "Steak", type: .dog),
+                Answer(text: "Fish", type: .cat),
+                Answer(text: "Salad", type: .rabbit)
+               ]
+             ),
+     
+     Question(text: "How much do you enjoy field trips?", type: .ranged,
+              Answer: [
+                Answer(text: "I love them!", type: .dog),
+                Answer(text: "I dislike them", type: .cat),
+                Answer(text: "I barely notice", type: .turtle),
+                Answer(text: "I geet a little nervous" , type: .rabbit)
+              ]
+             ),
+    
+     Question(text: "Which activity do you enjoy?", type: .multiple,
+              Answer: [
+                Answer(text: "Swimming", type: .turtle),
+                Answer(text: "Eating", type: .dog),
+                Answer(text: "Cuddling", type: .rabbit),
+                Answer(text: "Sleeping", type: .cat)
+              ]
+             )
+    
+    ]
+    
+    var questionIndex = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
